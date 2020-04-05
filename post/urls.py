a7920aa1bd0db7c2f549eb8ce95d8f2bf1a50439
g4mis0naw <gamisonaw@gmail.com>
2020-04-05 17:25:23 +0800

    add post

---
+++ post/urls.py
@@ -0,0 +1,31 @@
+"""post URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.http import HttpResponse, HttpResponseForbidden
+from django.urls import path, include
+from ratelimit.exceptions import Ratelimited
+
+urlpatterns = [
+    path('', include('message.urls')),
+    path('index', include('message.urls')),
+    path('index.html', include('message.urls')),
+]
+
+
+def handler403(request, exception=None):
+    if isinstance(exception, Ratelimited):
+        return HttpResponse('操作太快，先进小黑屋待一会吧', status=429)
+    return HttpResponseForbidden('Forbidden')
